57bfe8897aec8dda4e64b6cfeb752b18f11cc79c
이태한 <121335699+TaehanLee07@users.noreply.github.com>
2024-06-10 16:13:57 +0900

    Create 2743.java

---
+++ Java/Bronze/Bronze V/2743. 단어 길이 재기/2743.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc1 = new Scanner(System.in);
+        String str = sc1.nextLine();
+        System.out.println(str.length());
+    }
+}
