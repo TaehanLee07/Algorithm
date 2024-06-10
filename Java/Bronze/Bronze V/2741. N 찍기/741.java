7c51cc6f6222cbf889a25dcaf43bc09261e49414
이태한 <121335699+TaehanLee07@users.noreply.github.com>
2024-06-10 16:27:30 +0900

    Create 741.java

---
+++ Java/Bronze/Bronze V/2741. N 찍기/741.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+ 
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int N = sc.nextInt();
+        
+		int i = 1;
+        
+		while(i<= N) {
+			System.out.println(i);
+			i++;
+		}
+	}
+}
