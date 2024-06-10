663e08320d957e6e17f2452c64c0662a031b69bb
이태한 <121335699+TaehanLee07@users.noreply.github.com>
2024-06-10 15:32:56 +0900

    Create 2739.java

---
+++ Java/Bronze/Bronze V/2739. 구구단/2739.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int i = 1;
+
+        while (i <= 9) {
+            System.out.println(n + " * " + i + " = " + n * i);
+            i++;
+        }
+    }
+}
