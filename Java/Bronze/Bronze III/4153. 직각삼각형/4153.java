b2edc59be133d82c5c3af732132c1174cc200d40
이태한 <121335699+TaehanLee07@users.noreply.github.com>
2024-06-11 11:06:34 +0900

    Create 4153.java

---
+++ Java/Bronze/Bronze III/4153. 직각삼각형/4153.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+ 
+public class Main {
+	public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        while(true) {
+        	
+        	int x = in.nextInt();
+        	int y = in.nextInt();
+        	int z = in.nextInt();
+        	
+        	if(x == 0 && y == 0 && z == 0) break;
+          
+        	if((x * x + y * y) == z * z) {
+				System.out.println("right");
+			}
+        	else if(x * x == (y * y + z * z)) {
+				System.out.println("right");
+			}
+        	else if(y * y == (z * z + x * x)) {
+				System.out.println("right");
+			}
+        	else {
+				System.out.println("wrong");
+			}
+        	
+		}
+ 
+	}
+ 
+}
