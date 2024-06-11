52def486981fa17ce878ec9ad86b116eba13ae6a
이태한 <121335699+TaehanLee07@users.noreply.github.com>
2024-06-11 10:46:44 +0900

    Create 11720.java

---
+++ Java/Bronze/Bronze IV/11720. 숫자의 합/11720.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		String a = sc.next();
+		
+		int sum = 0;
+		
+		for (int i = 0; i < n; i++) {
+			sum += a.charAt(i)-'0';
+		}
+		System.out.println(sum);
+	}
+	
+}
