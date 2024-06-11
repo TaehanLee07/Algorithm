649bea252b63d20a6812f923c8d6dc7cec2a3272
이태한 <121335699+TaehanLee07@users.noreply.github.com>
2024-06-11 11:24:54 +0900

    Create 11718.java

---
+++ Java/Bronze/Bronze III/11718. 그대로 출력하기/11718.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String [] args) {
+        Scanner sc = new Scanner(System.in); 
+        
+        while (sc.hasNextLine()) {
+            String s = sc.nextLine();
+            System.out.println(s);
+        }
+        sc.close();
+     
+    }
+    
+}
