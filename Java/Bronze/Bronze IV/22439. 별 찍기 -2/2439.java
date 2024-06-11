1b70f6ceed48a81bf76698aee34887bdc99c2e8a
이태한 <121335699+TaehanLee07@users.noreply.github.com>
2024-06-11 10:42:36 +0900

    Create 2439.java

---
+++ Java/Bronze/Bronze IV/22439. 별 찍기 -2/2439.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int N = scanner.nextInt();
+
+        for (int i = 1; i <= N; i++) {
+            // 공백 출력
+            for (int j = 1; j <= N - i; j++) {
+                System.out.print(" ");
+            }
+            // 별 출력
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
