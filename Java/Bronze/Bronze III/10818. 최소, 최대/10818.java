501df1d5e5e76a48bdaab1f882f8be632feb0979
이태한 <121335699+TaehanLee07@users.noreply.github.com>
2024-06-11 11:19:27 +0900

    Create 10818.java

---
+++ Java/Bronze/Bronze III/10818. 최소, 최대/10818.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+ 
+ 
+public class Main {
+ 
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+ 
+		int N = Integer.parseInt(br.readLine());
+		StringTokenizer st = new StringTokenizer(br.readLine()," ");
+		
+		int index = 0;
+		int[] arr = new int[N];
+		while(st.hasMoreTokens()) {
+			arr[index] = Integer.parseInt(st.nextToken());
+			index++;
+		}
+		
+		Arrays.sort(arr);
+		System.out.print(arr[0] + " " + arr[N - 1]);
+	}
+}
