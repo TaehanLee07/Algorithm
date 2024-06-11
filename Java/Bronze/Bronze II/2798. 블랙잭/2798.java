95dba1a93df3b93b1e644adb1ab486afd3026f6b
이태한 <121335699+TaehanLee07@users.noreply.github.com>
2024-06-11 11:40:24 +0900

    Create 2798.java

---
+++ Java/Bronze/Bronze II/2798. 블랙잭/2798.java
@@ -0,0 +1,50 @@
+import java.util.Scanner;
+ 
+public class Main {
+	public static void main(String[] args) {
+ 
+		Scanner in = new Scanner(System.in);
+		
+		int N = in.nextInt();
+		int M = in.nextInt();
+ 
+		int[] arr = new int[N];
+ 
+		for (int i = 0; i < N; i++) {
+			arr[i] = in.nextInt();
+		}
+		
+		int result = search(arr, N, M);
+		System.out.println(result);
+	}
+ 
+	
+	static int search(int[] arr, int N, int M) {
+		int result = 0;
+ 
+		for (int i = 0; i < N - 2; i++) {
+			
+			if(arr[i] > M) continue;
+			
+			for (int j = i + 1; j < N - 1; j++) {
+ 
+				if(arr[i] + arr[j] > M) continue;
+				
+				for (int k = j + 1; k < N; k++) {
+					
+					int temp = arr[i] + arr[j] + arr[k];
+					
+					if (M == temp) {	
+						return temp;
+					}
+					
+					if(result < temp && temp < M) {
+						result = temp;
+					}
+				}
+			}
+		}
+		
+		return result;
+	}
+}
