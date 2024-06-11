691524ca3d329556c6ea920a7eddaf69d9393e6a
이태한 <121335699+TaehanLee07@users.noreply.github.com>
2024-06-11 11:22:56 +0900

    Create 10988.java

---
+++ Java/Bronze/Bronze III/10988. 팰린드롬인지 확인하기/10988.java
@@ -0,0 +1,30 @@
+import java.util.*;
+import java.io.*;
+	
+public class Main {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		String str = br.readLine();
+		
+		palindrome(str);
+		
+	} 
+	
+	static void palindrome(String str) {	
+		int length = str.length();
+		
+		for(int i=0; i<length/2; i++)	{
+			char front = str.charAt(i);
+			char back = str.charAt( (length-1)-i );
+			
+			if(front != back) {
+				System.out.println(0);
+				return;
+			}
+		}
+	
+		System.out.println(1);
+		
+	}
+} 
