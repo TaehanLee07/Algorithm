88b9b5d053a335c891bfc32e146734cc6850d252
이태한 <121335699+TaehanLee07@users.noreply.github.com>
2024-06-15 20:57:13 +0900

    Update 1214.java

--- Java/Platinum/Platinum V/1214. 쿨한 물건 구매/1214.java
+++ Java/Platinum/Platinum V/1214. 쿨한 물건 구매/1214.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int D = scanner.nextInt();
+        int D = scanner.nextInt(); 
         int P = scanner.nextInt();
         int Q = scanner.nextInt();
 
