021b27bd16885b869b9835d6e78c2129e781f467
이태한 <121335699+TaehanLee07@users.noreply.github.com>
2024-06-17 08:32:06 +0900

    Create 11047.java

---
+++ Java/Silver/Silver IV/11047. 동전 0/11047.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+ 
+public class Main {
+ 
+	public static void main(String[] args) {
+ 
+		Scanner sc = new Scanner(System.in);
+		
+		int N = sc.nextInt();
+		int K = sc.nextInt();
+		
+		int[] coin = new int[N];
+		
+		for(int i = 0; i < N; i++) {
+			coin[i] = sc.nextInt();
+		}
+		
+		int count = 0;
+ 
+		for(int i = N - 1; i >= 0; i--) {
+ 
+			// 현재 동전의 가치가 K보다 작거나 같아야지 구성가능하다.
+			if(coin[i] <= K) {
+				// 현재 가치의 동전으로 구성할 수 있는 개수를 더해준다.
+				count += (K / coin[i]);
+				K = K % coin[i];
+			}
+		}
+		System.out.println(count);
+	}
+ 
+}
