0658d5b1d114df13cab786bf953c573416844054
이태한 <121335699+TaehanLee07@users.noreply.github.com>
2024-06-29 23:51:33 +0900

    Update 12865.java

--- Java/Gold/Gold V/12865. 평범한 배낭/12865.java
+++ Java/Gold/Gold V/12865. 평범한 배낭/12865.java
@@ -6,10 +6,8 @@ import java.util.StringTokenizer;
 public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
 		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-		
-		
+
 		int N = Integer.parseInt(st.nextToken());
 		int K = Integer.parseInt(st.nextToken());
  
