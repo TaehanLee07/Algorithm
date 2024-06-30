bce3411a4f141ea7db9574ff3b97e1ef990e6c37
이태한 <121335699+TaehanLee07@users.noreply.github.com>
2024-07-01 08:36:28 +0900

    Create 9663.java

---
+++ Java/Gold/Gold IV/9663. N-Queen/9663.java
@@ -0,0 +1,41 @@
+import java.io.*;
+ 
+public class Main {
+    static int N, cnt, chess[];
+ 
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(bf.readLine());
+        chess = new int[N];
+ 
+        dfs(0);
+        System.out.println(cnt);
+    }
+ 
+    private static void dfs(int cur) {
+        if (cur == N) {
+            cnt++;
+            return;
+        }
+ 
+        for (int i = 0; i < N; i++) {
+            chess[cur] = i;
+            if (isOk(cur)) {
+                dfs(cur + 1);
+            }
+        }
+    }
+ 
+    private static boolean isOk(int col) {
+        for (int i = 0; i < col; i++) {
+            // 같은 행에 존재하는 경우
+            if (chess[col] == chess[i])
+                return false;
+ 
+            // 같은 대각선에 존재하는 경우
+            if (col - i == Math.abs(chess[col] - chess[i]))
+                return false;
+        }
+        return true;
+    }
+}
