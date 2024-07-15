e4a9a96ec25074f3cffc194ed3e68c09e3592728
이태한 <121335699+TaehanLee07@users.noreply.github.com>
2024-07-15 21:07:59 +0900

    Create 9295.java

---
+++ Java/Bronze/Bronze IV/9295. 주사위/9295.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int T = sc.nextInt();
+        int[] arr = new int[T];
+
+        for (int i = 0; i < arr.length; i++) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            arr[i] = a + b;
+        }
+        for (int i = 1; i <= T; i++) {
+            System.out.println("Case " + i + ": " + arr[i - 1]);
+        }
+    }
+}
