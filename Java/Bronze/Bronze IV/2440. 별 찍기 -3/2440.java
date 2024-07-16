99307af82992ef790dbfb34d69ba96a15e6b3de1
이태한 <121335699+TaehanLee07@users.noreply.github.com>
2024-07-16 21:50:20 +0900

    Update 2440.java

--- Java/Bronze/Bronze IV/2440. 별 찍기 -3/2440.java
+++ Java/Bronze/Bronze IV/2440. 별 찍기 -3/2440.java
@@ -1,8 +1,7 @@
 import java.util.*;
  
 public class Main {
-    public static void main(String[] args){
-        
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         
         int num = sc.nextInt();
