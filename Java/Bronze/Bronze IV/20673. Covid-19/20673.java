01b5faa683534ddf6474ea346bed9ecfcf7b036d
이태한 <121335699+TaehanLee07@users.noreply.github.com>
2024-07-19 15:13:28 +0900

    Create 20673.java

---
+++ Java/Bronze/Bronze IV/20673. Covid-19/20673.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Main{
+	public static void main(String args[]){
+		Scanner sc = new Scanner(System.in);
+		int p, q;
+		p = sc.nextInt();
+		q = sc.nextInt();
+		
+        if (p <= 50 && q <= 10)
+            System.out.println("White");
+        else if (q > 30)
+            System.out.println("Red");
+        else
+            System.out.println("Yellow");
+	}
+}
