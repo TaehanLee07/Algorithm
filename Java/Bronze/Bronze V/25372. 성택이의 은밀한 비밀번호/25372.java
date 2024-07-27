e37d182151eb8894827199b3c7c6b9745a095c1c
이태한 <121335699+TaehanLee07@users.noreply.github.com>
2024-07-27 16:47:37 +0900

    Create 25372.java

---
+++ Java/Bronze/Bronze V/25372. 성택이의 은밀한 비밀번호/25372.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        StringBuilder sb = new StringBuilder();
+
+        int N = sc.nextInt();
+
+        for(int i = 0 ; i < N ; i ++){
+            String s = sc.next();
+
+            if(s.length() >= 10 || s.length() < 6)
+                sb.append("no");
+            else
+                sb.append("yes");
+            sb.append("\n");
+        }
+        System.out.print(sb);
+    }
+}
