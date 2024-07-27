3affb4aaa64b64a90f776dbf2789c9587072d66d
이태한 <121335699+TaehanLee07@users.noreply.github.com>
2024-07-27 16:51:19 +0900

    Update 29751.java

--- Java/Bronze/Bronze V/29751. 삼각형/29751.java
+++ Java/Bronze/Bronze V/29751. 삼각형/29751.java
@@ -3,9 +3,9 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        
         double W = sc.nextDouble();
         double H = sc.nextDouble();
-        
         double res = (W * H) / 2; 
         
         System.out.printf("%.1f", res);
