e915313b22fce837cde25226712f7cf5b4f2e295
이태한 <121335699+TaehanLee07@users.noreply.github.com>
2024-07-27 16:48:11 +0900

    Update 25372.java

--- Java/Bronze/Bronze V/25372. 성택이의 은밀한 비밀번호/25372.java
+++ Java/Bronze/Bronze V/25372. 성택이의 은밀한 비밀번호/25372.java
@@ -1,7 +1,7 @@
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         StringBuilder sb = new StringBuilder();
 
