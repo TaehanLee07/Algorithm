b4617374a34632692d36968180f31bcec2a4fc45
이태한 <121335699+TaehanLee07@users.noreply.github.com>
2024-07-28 20:18:42 +0900

    Create 28431.java

---
+++ Java/Bronze/Bronze IV/28431. 양말 짝 맞추기/28431.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class Main {
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+        int[] socks = new int[5];
+        int index;
+        int cnt = 0;
+        
+		for(int i = 0; i < socks.length; i++)
+			socks[i] = sc.nextInt();
+		
+		for(index = 0; index < 5; index++) {
+			for(int j = 0; j < 5; j++) 
+				if(socks[index] == socks[j])
+					cnt++;
+			if(cnt % 2 == 1)
+				break;
+		}
+        
+		System.out.println(socks[index]);
+	}
+}
