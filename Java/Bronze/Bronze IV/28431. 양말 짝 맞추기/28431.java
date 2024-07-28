33f2d83d70a37b2b24af757c25aeea2d1c4607f5
이태한 <121335699+TaehanLee07@users.noreply.github.com>
2024-07-28 20:19:29 +0900

    Update 28431.java

--- Java/Bronze/Bronze IV/28431. 양말 짝 맞추기/28431.java
+++ Java/Bronze/Bronze IV/28431. 양말 짝 맞추기/28431.java
@@ -11,14 +11,13 @@ public class Main {
 	for(int i = 0; i < socks.length; i++)
 		socks[i] = sc.nextInt();
 		
-		for(index = 0; index < 5; index++) {
-			for(int j = 0; j < 5; j++) 
-				if(socks[index] == socks[j])
-					cnt++;
-			if(cnt % 2 == 1)
-				break;
+	for(index = 0; index < 5; index++) {
+		for(int j = 0; j < 5; j++) 
+			if(socks[index] == socks[j])
+				cnt++;
+		if(cnt % 2 == 1)
+			break;
 		}
-        
 		System.out.println(socks[index]);
 	}
 }
