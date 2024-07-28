2f5867779fec3506bd28f3048f6cb1b43a8b7f85
이태한 <121335699+TaehanLee07@users.noreply.github.com>
2024-07-28 20:18:50 +0900

    Update 28431.java

--- Java/Bronze/Bronze IV/28431. 양말 짝 맞추기/28431.java
+++ Java/Bronze/Bronze IV/28431. 양말 짝 맞추기/28431.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 public class Main {
 	public static void main(String[] args) {
 		
-		Scanner sc = new Scanner(System.in);
+	Scanner sc = new Scanner(System.in);
         int[] socks = new int[5];
         int index;
         int cnt = 0;
