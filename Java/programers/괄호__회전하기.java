53e0bc0c419b623d55142a37752b18fefee5c75a
이태한 <121335699+TaehanLee07@users.noreply.github.com>
2024-08-26 17:57:25 +0900

    Create 괄호__회전하기.java

---
+++ Java/programers/괄호__회전하기.java
@@ -0,0 +1,15 @@
+package programers;
+
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class 괄호__회전하기 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String[] st = sc.next().split(" ");
+        String correctWord[] = {"()", "{}", "[]"};
+
+        for (int i = 0; i < st.length; i++) {
+        }
+    }
+}
